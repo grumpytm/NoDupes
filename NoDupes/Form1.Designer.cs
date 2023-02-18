8c3aface7fff05b30aee7b989b3cdcdd6d64e9da
Mike Roch <cezarica01@yahoo.com>
2023-02-18 17:10:17 +0200

    Add project files.

---
+++ NoDupes/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace NoDupes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonDataGridView1 = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
+            this.kryptonButton2 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonDataGridView1
+            // 
+            this.kryptonDataGridView1.AllowUserToAddRows = false;
+            this.kryptonDataGridView1.AllowUserToDeleteRows = false;
+            this.kryptonDataGridView1.AllowUserToOrderColumns = true;
+            this.kryptonDataGridView1.AllowUserToResizeColumns = false;
+            this.kryptonDataGridView1.AllowUserToResizeRows = false;
+            this.kryptonDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.kryptonDataGridView1.GridStyles.Style = ComponentFactory.Krypton.Toolkit.DataGridViewStyle.Mixed;
+            this.kryptonDataGridView1.GridStyles.StyleBackground = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.GridBackgroundSheet;
+            this.kryptonDataGridView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.kryptonDataGridView1.Location = new System.Drawing.Point(25, 65);
+            this.kryptonDataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.kryptonDataGridView1.Name = "kryptonDataGridView1";
+            this.kryptonDataGridView1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalOffice2003;
+            this.kryptonDataGridView1.ReadOnly = true;
+            this.kryptonDataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.kryptonDataGridView1.RowHeadersVisible = false;
+            this.kryptonDataGridView1.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.kryptonDataGridView1.RowTemplate.ReadOnly = true;
+            this.kryptonDataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.kryptonDataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.kryptonDataGridView1.Size = new System.Drawing.Size(801, 470);
+            this.kryptonDataGridView1.StandardTab = true;
+            this.kryptonDataGridView1.TabIndex = 0;
+            this.kryptonDataGridView1.TabStop = false;
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.Location = new System.Drawing.Point(736, 552);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.kryptonButton2.Size = new System.Drawing.Size(90, 25);
+            this.kryptonButton2.TabIndex = 70;
+            this.kryptonButton2.TabStop = false;
+            this.kryptonButton2.Values.Text = "Refresh";
+            this.kryptonButton2.Click += new System.EventHandler(this.kryptonButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 600);
+            this.Controls.Add(this.kryptonButton2);
+            this.Controls.Add(this.kryptonDataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "NoDupes";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private ComponentFactory.Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton2;
+    }
+}
+
---
+++ NoDupes/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+/* 3rd party libs */
+using Ganss.Excel;
+using MetroFramework.Forms;
+using System.Drawing;
+
+namespace NoDupes
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fillGrid();
+
+            kryptonDataGridView1.RowsDefaultCellStyle.SelectionBackColor = Color.FromArgb(255, 234, 234, 234);
+            kryptonDataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+        }
+
+        public class Product
+        {
+            [Column("Cod")]
+            public string Cod { get; set; }
+            [Column("Denumire")]
+            public string Denumire { get; set; }
+            [Column("UM")]
+            public string Unitate { get; set; }
+            [Column("Cant.")]
+            public int Cant { get; set; }
+            [Column("PU")]
+            public string Pret { get; set; }
+        }
+
+        private void fillGrid()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Cod", typeof(string));
+            dt.Columns.Add("Denumire", typeof(string));
+            //dt.Columns.Add("UM", typeof(string));
+            dt.Columns.Add("Cant", typeof(int));
+            dt.Columns.Add("PU", typeof(string));
+
+            var path = Environment.CurrentDirectory;
+            var files = Directory.EnumerateFiles(path).Select(p => Path.GetFileName(p))
+                .Where(file => new string[] { ".xls", ".xlsx" }.Contains(Path.GetExtension(file)))
+                .Select(fn => Path.GetFileName(fn));
+
+            foreach (var file in files)
+            {
+                var products = new ExcelMapper(file).Fetch<Product>();
+
+                foreach (var product in products)
+                {
+                    //dt.Rows.Add(product.Cod, product.Denumire, product.Unitate, product.Cant, product.Pret);
+                    dt.Rows.Add(product.Cod, product.Denumire, product.Cant, product.Pret);
+                }
+            }
+
+            var groups = dt.AsEnumerable()
+                .GroupBy(x => new {
+                    id1 = x.Field<string>("Cod"),
+                    id2 = x.Field<string>("Denumire"),
+                    //id3 = x.Field<string>("UM"),
+                    id3 = x.Field<string>("PU")
+                }).ToList();
+
+            DataTable results = dt.Clone();
+
+            foreach (var group in groups)
+            {
+                //results.Rows.Add(new object[] { group.Key.id1, group.Key.id2, group.Key.id3, group.Sum(x => x.Field<int>("Cant")), group.Key.id4 });
+                results.Rows.Add(new object[] { group.Key.id1, group.Key.id2, group.Sum(x => x.Field<int>("Cant")), group.Key.id3 });
+            }
+
+            kryptonDataGridView1.DataSource = results;
+            kryptonDataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            kryptonDataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            kryptonDataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            kryptonDataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            //kryptonDataGridView1.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            kryptonDataGridView1.Columns[0].Width = 100;
+            kryptonDataGridView1.Columns[1].Width = 500;
+            kryptonDataGridView1.Columns[2].Width = 50;
+            //kryptonDataGridView1.Columns[3].Width = 50;
+            kryptonDataGridView1.Columns[3].Width = 100;
+
+            GC.Collect();
+        }
+
+        private void kryptonButton2_Click(object sender, EventArgs e)
+        {
+            fillGrid();
+        }
+    }
+}
